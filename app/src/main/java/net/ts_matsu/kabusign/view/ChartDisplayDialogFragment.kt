28a950d6ef18df067ddf52b9108e9981a0b64874
ts-matsu <dev-app@ts-matsu.net>
2022-01-30 16:15:34 +0900

    ひとまずダイアログにチャート表示できた版

--- app/src/main/java/net/ts_matsu/kabusign/view/ChartDisplayDialogFragment.kt
+++ app/src/main/java/net/ts_matsu/kabusign/view/ChartDisplayDialogFragment.kt
@@ -2,20 +2,32 @@ package net.ts_matsu.kabusign.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import net.ts_matsu.kabusign.R
 import net.ts_matsu.kabusign.viewmodel.ChartDisplayDialogViewModel
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
+import com.github.mikephil.charting.charts.CombinedChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import net.ts_matsu.kabusign.databinding.ChartDisplayDialogFragmentBinding
 import net.ts_matsu.kabusign.databinding.FragmentStockNotificationDialogBinding
+import net.ts_matsu.kabusign.util.CommonInfo
 
 class ChartDisplayDialogFragment : DialogFragment() {
+    private val cName = ConditionMainFragment::class.java.simpleName
+
     private val viewModel = ChartDisplayDialogViewModel()
     private lateinit var binding: ChartDisplayDialogFragmentBinding
+    private val args: ChartDisplayDialogFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.StockChartDialogTheme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,5 +41,87 @@ class ChartDisplayDialogFragment : DialogFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // 銘柄名、株価の2行の高さの、全体の高さに対する割合
+        // ひとまず 80dp としている
+        val topMarginPixel = 80 * resources.displayMetrics.density
+
+        // ダイアログの位置を調整
+        dialog?.window?.attributes.also {
+            it?.gravity = Gravity.TOP
+            it?.verticalMargin = topMarginPixel/resources.displayMetrics.heightPixels
+        }
+
+        CommonInfo.debugInfo("aaaa: ${topMarginPixel/resources.displayMetrics.heightPixels}")
+
+        // ダイアログの高さ/幅を設定
+        // 幅は、Styleで設定できるが、高さがうまく設定できないので、ここで設定する
+        dialog?.window?.setLayout(resources.displayMetrics.widthPixels, resources.displayMetrics.heightPixels - topMarginPixel.toInt())
+
+
+        // 戻ってきたときかどうかをどうやって区別するのか？
+        // （デフォルト値が""なので、それで区別できる）
+        if(args.code.isNotEmpty()){
+            viewModel.setStockItem(args.code)
+        }
+
+        // チャートデータの監視
+        viewModel.chartData.observe(viewLifecycleOwner, Observer {
+            showChart()
+        })
+
+        // ConbinedChartの初期設定
+        val combinedChart = binding.combinedChart
+        combinedChart.apply {
+            setTouchEnabled(true)
+            isDragEnabled = true
+            isScaleXEnabled = true
+            isScaleYEnabled = true
+            drawOrder = arrayOf(CombinedChart.DrawOrder.CANDLE, CombinedChart.DrawOrder.LINE)               // 移動平均線を後から描画する
+            setNoDataText("")
+
+            // チャートタップ時の、横線表示
+            setOnTouchListener { view, motionEvent ->
+                view.performClick()     // これをコールしろとメッセージが出るためコールしている
+
+                // タップ時のみ横線描画
+                if(motionEvent.action == MotionEvent.ACTION_DOWN){
+                    val pointY = getTransformer(YAxis.AxisDependency.LEFT).getValuesByTouchPoint(motionEvent.x, motionEvent.y).y.toFloat()
+                    showLimitLine(pointY)
+                }
+                // true を返すと、ここの処理しかされなくて、Zoom処理とかが行えなかった。
+                false
+            }
+        }
+
+    }
+
+    // MPAndroidChartは、データバインディングサポートしてないので、
+    // ここにチャート表示ロジックを記載する必要がある。
+    private fun showChart(){
+        CommonInfo.debugInfo("$cName: showChart")
+        val combinedChart = binding.combinedChart
+        combinedChart.apply {
+            combinedChart.description.text = ""
+            combinedChart.legend.isEnabled = false                      // 凡例は表示しない
+            combinedChart.xAxis.position = XAxis.XAxisPosition.BOTTOM   // X軸ラベルは下側
+            combinedChart.axisLeft.setDrawLabels(false)                 // Y軸ラベルは右側のみ
+            combinedChart.xAxis.setLabelCount(5,false)      // X軸の表示ラベル数
+            combinedChart.data = viewModel.chartData.value
+            combinedChart.xAxis.valueFormatter = IndexAxisValueFormatter(viewModel.dateData.value)
+            combinedChart.notifyDataSetChanged()
+            combinedChart.invalidate()
+        }
+    }
+
+    private fun showLimitLine(yValue: Float){
+        val value = yValue.toInt().toFloat()    // 整数に変換
+        viewModel.getLimitLine(value)?.let {
+            val combinedChart = binding.combinedChart
+            val axisRight = combinedChart.axisRight
+            axisRight.removeAllLimitLines()
+            axisRight.addLimitLine(it)
+            combinedChart.notifyDataSetChanged()
+            combinedChart.invalidate()
+        }
     }
 }
--- app/src/main/java/net/ts_matsu/kabusign/viewmodel/ChartDisplayDialogViewModel.kt
+++ app/src/main/java/net/ts_matsu/kabusign/viewmodel/ChartDisplayDialogViewModel.kt
@@ -1,11 +1,86 @@
 package net.ts_matsu.kabusign.viewmodel
 
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.data.CombinedData
+import kotlinx.coroutines.launch
+import net.ts_matsu.kabusign.model.StockChart
+import net.ts_matsu.kabusign.model.StockDataManager
+import net.ts_matsu.kabusign.util.CommonInfo
 
 class ChartDisplayDialogViewModel : ViewModel() {
     private val cName = ChartDisplayDialogViewModel::class.java.simpleName
+
+    // チャートデータ
+    private val _chartData = MutableLiveData<CombinedData>()
+    val chartData: LiveData<CombinedData> get() = _chartData
+
+    // チャートデータに対応した日付データ
+    private val _dateData = MutableLiveData<MutableList<String>>()
+    val dateData: LiveData<MutableList<String>> get() = _dateData
+
+    // 横線描画用アイコンの状態
+    private val _isEditImageButton = MutableLiveData<Boolean>(false)
+    val isEditImageButton: LiveData<Boolean> get() = _isEditImageButton
+
+    // チャート更新時のプログレスバー表示
+    private val _isUpdateProgress = MutableLiveData<Boolean>(false)
+    val isUpdateProgress: LiveData<Boolean> get() = _isUpdateProgress
+
+    // 保持する各種データ
+    var currentStockCode = ""                                               // 現在表示中の銘柄コード
+    private val chartList = mutableListOf<StockChart>()                     // 各銘柄のチャート情報
+
+    // 銘柄情報（コード）を設定する
+    fun setStockItem(code: String){
+//        val code = item.substring(item.length-5, item.length-1)     // "${info.name} (${info.code})" の形式になっている
+        CommonInfo.debugInfo("$cName: setStockItem($code), ${code.isDigitsOnly()}")
+        if(code.isNotEmpty() && code.isDigitsOnly()){
+            currentStockCode = code
+            setChartData(true)
+        }
+    }
+
+    // 横線情報取得
+    fun getLimitLine(data: Float): LimitLine?{
+        // 横線描画OKの場合のみ設定する
+        var result: LimitLine? = null
+        if(currentStockCode.isNotEmpty() && _isEditImageButton.value!!){
+            for(d in chartList){
+                if(d.stockCode == currentStockCode){
+                    result = d.getLimitLineData(data)
+                }
+            }
+        }
+        return result
+    }
+
+    // チャートデータを設定する
+    // どの銘柄のどのチャートを表示するかは、ViewModelが握っている
+    private fun setChartData(isUpdate: Boolean) {
+        viewModelScope.launch {
+            CommonInfo.debugInfo("$cName: setChartData")
+            _isUpdateProgress.value = true        // プログレスバー開始
+
+            val stockDataManager = StockDataManager()
+            val data = stockDataManager.getDataList(currentStockCode, isUpdate)
+            // chartList に追加
+            val stockChart = StockChart(currentStockCode)
+            chartList.add(stockChart)
+
+            // グラフ情報更新
+            CommonInfo.debugInfo("size: ${data.size}")
+            _dateData.value = stockChart.getDateList(data)
+            _chartData.value = stockChart.getChartData(data)
+            CommonInfo.debugInfo("$cName: setChartData2")
+            _isUpdateProgress.value = false        // プログレスバー終了
+        }
+        CommonInfo.debugInfo("$cName: setChartData3")
+    }
+
 
 }
